8974a5fa6b0c9949c51f3f9e740b9c5d751043c8
thejgamer23 <141654756+thejgamer23@users.noreply.github.com>
2025-03-01 18:04:21 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 lua54 'yes'
 
 author 'TheJGamer'
-description 'This Is a spawn script to clean up spawn areas https://tjg-modiforcations.gitbook.io/tjg-modifications'
+description 'This Is a spawn script to clean up spawn areas https://github.com/thejgamer23/TJG-spawn'
 version '1.0.0'
 
 Resource_type 'gametype' {Name = 'TJG Spawn!', }
